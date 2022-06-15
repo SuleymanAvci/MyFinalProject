f921fe944a064b3bbd88e6da7bf8d7ba0d22c631
sa1avci <sa1avci@hotmail.com>
2022-06-16 00:09:27 +0300

    Dto eklendi

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal CategoryDal)
+        {
+            _categoryDal = CategoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int CategoryId)
+        {
+            return _categoryDal.Get(c => c.CategoryId == CategoryId);
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,13 +9,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
 
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var item in productManager.GetAll())
+            foreach (var item in productManager.GetProductDetails())
             {
-                Console.WriteLine(item.ProductName);
-                Console.WriteLine(item.UnitPrice);
+                Console.WriteLine(item.ProductName + " / " + item.CategoryName);
+
 
             }
         }
